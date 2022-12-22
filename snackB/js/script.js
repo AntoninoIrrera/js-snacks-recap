3f259ff1ebf191938ec4cc2d97b16c001c3271d6
AntoninoIrrera <irrera.antonio2003@gmail.com>
2022-12-22 15:36:08 +0100

    correzione piccolo errore tecnico snack B

--- snackB/js/script.js
+++ snackB/js/script.js
@@ -6,7 +6,7 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            userInput: "",
+            userInput: null,
             numbers: null,
             somma: 0,
         }
@@ -15,21 +15,25 @@ createApp({
 
         getRandomNumbers() {
 
-            axios.get(`https://flynn.boolean.careers/exercises/api/array/integers?min=1&max=100&items=${this.userInput}`)
-                .then((response) => {
-                    this.somma = 0;
+            if(this.userInput > 0 && this.userInput <= 100){
 
+                axios.get(`https://flynn.boolean.careers/exercises/api/array/integers?min=1&max=100&items=${this.userInput}`)
+                .then((response) => {
+                    
+                    this.somma = 0;
+                    
                     this.numbers = response.data.response;
                     console.log(this.numbers);
-
+                    
                     this.numbers.forEach(number => {
-
+                        
                         this.somma = this.somma + number;
-
+                        
                     });
-
-
-                })
+                    
+                    
+                });
+            }
         }
 
 
